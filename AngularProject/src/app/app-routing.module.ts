d5a197d790a5a48f35d94c6f0617a141c783a223
Erik Obst <erik.obst@iu-study.org>
2023-03-22 08:10:02 +0100

    Creating Routes to the Landing-,Imprint-,Privacy- and RegistrationPage

--- AngularProject/src/app/app-routing.module.ts
+++ AngularProject/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImprintPageComponent } from './Pages/imprint-page/imprint-page.component';
+import { LandingPageComponent } from './Pages/landing-page/landing-page.component';
+import { PrivacyPageComponent } from './Pages/privacy-page/privacy-page.component';
+import { RegistrationPageComponent } from './Pages/registration-page/registration-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:LandingPageComponent},
+  {path: "anmeldung", component: RegistrationPageComponent},
+  {path: "impressum", component: ImprintPageComponent},
+  {path: "datenschutz", component: PrivacyPageComponent},
+  {path: "**", redirectTo: "/"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
