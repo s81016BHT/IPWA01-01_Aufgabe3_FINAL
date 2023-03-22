0c80868d9ce9dc4dab87da0452d95946bc56585f
Erik Obst <erik.obst@iu-study.org>
2023-03-22 08:04:46 +0100

    Adding Components and Pages Folder to AngularProject. Also creating initial important components for Pages and global header/footer component

---
+++ AngularProject/src/app/Pages/imprint-page/imprint-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imprint-page',
+  templateUrl: './imprint-page.component.html',
+  styleUrls: ['./imprint-page.component.css']
+})
+export class ImprintPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- AngularProject/src/app/app.module.ts
+++ AngularProject/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './Pages/landing-page/landing-page.component';
+import { ImprintPageComponent } from './Pages/imprint-page/imprint-page.component';
+import { PrivacyPageComponent } from './Pages/privacy-page/privacy-page.component';
+import { RegistrationPageComponent } from './Pages/registration-page/registration-page.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ImprintPageComponent,
+    PrivacyPageComponent,
+    RegistrationPageComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
