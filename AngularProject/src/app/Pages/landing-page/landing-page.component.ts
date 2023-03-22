bc0f2a1772537f3bb6d481ccfee2689dcd4909d7
Erik Obst <erik.obst@iu-study.org>
2023-03-22 09:37:25 +0100

    Adding Landing Page Content

--- AngularProject/src/app/Pages/landing-page/landing-page.component.ts
+++ AngularProject/src/app/Pages/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { texts } from './pagetexts';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class LandingPageComponent implements OnInit {
 
   constructor() { }
+
+  public texts = texts
 
   ngOnInit(): void {
   }
---
+++ AngularProject/src/app/Pages/landing-page/pagetexts.ts
@@ -0,0 +1,24 @@
+export const texts = {
+    header_title: 'Kleidung spenden,\ndirekt helfen!',
+    header_text: 'Egal ob Türkei, Syrien Indien oder ein anderes Krisengebiet. Spende jetzt Kleidung und entscheide selbst, wohin sie geht!',
+
+    button_more: "Mehr erfahren",
+    button_donate: "Jetzt Kleidung spenden!",
+
+    imagebox1_title : "Türkei",
+    imagebox1_text : `Nach den schweren Erdbeben in der Türkei ab dem 06.02.2023 in der Region um Adana ist vielerorts nicht viel von der bekannten Umgebung übrig geblieben. Viele Menschen haben durch dieses Naturereignis ihre Heimat verloren und die Sehnsucht nach Perspektiven für die Zukunft ist groß. Nach aktuellen Zahlen zählt man alleine in der Türkei mindestens 50.000 Tote und um die 214.000 eingestürzte bzw. einsturzgefärdete Gebäude. Spenden sie Kleidung und Wärme und helfen Sie so den Menschen vor Ort!`,
+
+    imagebox2_title : "Indische Slums",
+    imagebox2_text : `Das Leben in den Armenvirteln in Indien, den sogenannten indisches Slums ist hart und beschwerlich. Längst hält sich der Glaube, dass man aus den Slums nicht herauskommt. Diverse Aktionen von gemeinnützigen Organisationen wie Terra Tech haben es sich zur Aufgabe gemacht, das Leben vor Ort für die Kinder und Erwachsenen nachhaltig zu verbessern. Auch wir leisten unseren Beitrag, indem wir Kleidung aus Spenden an die ärmsten abgeben. Du willst helfen? Dann spende jetzt Kleidung nach Indien!`,
+
+    videobox_text : `In dieser Reportage von ZDFHeute wird das Ausmaß der Erbebenkatastrophe in der Türkei und Syrien ersichtlich. Ein Land welches bei der humanitären Hilfe sehr häufig vergessen wird ist Syrien. Syrien ist ein Land, welches unter anderem durch Terror und Kriege ohnehin gebeutelt ist. Das Erbeben zuletzt hat die Situation zusätzlich noch schwieriger gestaltet\n\nLassen Sie uns gemeinsam helfen. Durch eine einmalige Kleiderspende können Sie einen Beitrag dazu leisten, die humanitäre Krise aufzulösen und den Leuten vor Ort helfen.`,
+
+    stats_percentage1: 33,
+    stats_percentage1_text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor \n\n Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor",
+
+    stats_percentage2: 42,
+    stats_percentage2_text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor \n\n Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor",
+
+    stats_percentage3: 16,
+    stats_percentage3_text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor \n\n Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor",
+};
