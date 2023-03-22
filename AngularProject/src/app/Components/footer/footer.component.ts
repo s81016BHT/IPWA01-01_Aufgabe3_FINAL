d99a1b2201e77331eebd83fc79783b98c12fe889
Erik Obst <erik.obst@iu-study.org>
2023-03-22 08:59:04 +0100

    Adding footer

--- AngularProject/src/app/Components/footer/footer.component.ts
+++ AngularProject/src/app/Components/footer/footer.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router, NavigationEnd} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent{
+  routes = [
+    { path:"/impressum", name:"Impressum",state:false},
+    { path:"/datenschutz", name:"Datenschutzerklärung",state:false}
+  ]
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router : Router){
+    router.events.subscribe((event : any) => {
+      if(event instanceof NavigationEnd){
+        this.routes.forEach(route => {
+          if(event.url == route.path) route.state = true;
+          else route.state = false;
+        });
+      }
+    });
   }
-
 }
