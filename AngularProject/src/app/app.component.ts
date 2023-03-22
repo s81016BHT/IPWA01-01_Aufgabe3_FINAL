4dad7ac3c14174fa6a492939660bfe779e883263
Erik Obst <erik.obst@iu-study.org>
2023-03-22 09:21:19 +0100

    Adding scrollTop feature on route change

--- AngularProject/src/app/app.component.ts
+++ AngularProject/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularProject';
+  scrollTop(){
+    document.getElementsByTagName("body")[0].scrollTo(0,0);
+  }
 }
